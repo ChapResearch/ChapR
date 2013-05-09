c77f4d18d7de26a281c3e376d1d21e013af6e6f9
rachelgardner9@gmail.com <rachelgardner9@gmail.com@64f68712-5ba5-a463-617d-d4db94448ed3>
2013-05-09 18:48:04 +0000

    Added the new button class, as well as the test results from 5-9-13. Holding onto the Chapr.ino file because it may have some bugs.

---
+++ trunk/ChaprSVN/Arduino/Chapr/button.h
@@ -0,0 +1,20 @@
+
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+class button
+{
+ public:
+  button(int pin);
+  
+  bool isPressed();
+  bool hasChanged();
+
+ private:
+  int _pin;
+  bool wasPressed;
+};
+
+#endif BUTTON_H
+
