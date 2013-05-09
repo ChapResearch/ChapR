504d3e6b829c9bd29c33b368aee86f740bae2328
rachelgardner9 <rachelgardner9@gmail.com>
2013-05-09 18:48:04 +0000

    Added the new button class, as well as the test results from 5-9-13. Holding onto the Chapr.ino file because it may have some bugs.

---
+++ ChaprSVN/Arduino/Chapr/button.cpp
@@ -0,0 +1,26 @@
+//this is the button class
+
+#include <Arduino.h>
+#include "button.h"
+
+button::button (int pin) : _pin(pin), wasPressed(false)
+{
+  pinMode(_pin, INPUT);
+}
+
+bool button::isPressed()
+{
+  wasPressed = digitalRead(_pin);
+  return wasPressed;
+}
+
+bool button::hasChanged()
+{
+  int current = digitalRead(_pin);
+  if (current != wasPressed){
+    wasPressed = current;
+    return true;
+  }
+  wasPressed = current;
+  return false;
+}
