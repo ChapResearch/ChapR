33cd141808b4a1a873871c9300f95f3d8a049de9
sidharths00@gmail.com <sidharths00@gmail.com@64f68712-5ba5-a463-617d-d4db94448ed3>
2015-01-18 18:08:21 +0000

    added doPiano and sounds for it

---
+++ trunk/ChaprSVN/ChapX/doPiano__/doPiano__.ino
@@ -0,0 +1,13 @@
+void loop(){
+  doPiano();
+}
+
+void doPiano(){
+  int note = readTouchInput;
+  for(int x = 0; x < 12; x++){
+    if(note == x){
+      playNoise('n',x);
+      delay(500);
+    }
+  }
+}
