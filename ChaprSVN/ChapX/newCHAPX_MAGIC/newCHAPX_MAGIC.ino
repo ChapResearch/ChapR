c0ee511d7469d11a0775728ebaf13279f7ac1532
rachelgardner9 <rachelgardner9@gmail.com>
2015-02-12 01:22:25 +0000

    A bug in new magic new is still there. When played, the arduino senses a fruit being touched before you can touch the correct fruit.

--- ChaprSVN/ChapX/newCHAPX_MAGIC/newCHAPX_MAGIC.ino
+++ ChaprSVN/ChapX/newCHAPX_MAGIC/newCHAPX_MAGIC.ino
@@ -5,17 +5,14 @@
 
 SoftwareSerial mySerial(12, 11); // RX, TX (flipped from docs)
 
+int irqpin = 2;  // Digital 2
 boolean touchStates[12]; //to keep track of the previous touch states
 int RTS = 0;
-volatile boolean touch_status_changed =false;
-void touch_irq()
-{
-  touch_status_changed=true;
-}
-void setup(){
 
-  attachInterrupt(0, touch_irq, FALLING);
- 
+void setup(){
+  pinMode(irqpin, INPUT);
+  digitalWrite(irqpin, HIGH); //enable pullup resistor
+  
   pinMode(8, INPUT);
   digitalWrite(8, HIGH);
   
@@ -40,13 +37,11 @@ boolean pressStart = true;
 
 // g0 presstoplay g1 goodsound g2 badsound g3 newhighscore g4 your score is  g5 wah wah wah waaaaaaahhh g6 silence
 void loop(){
-
     if(pressStart){
         Serial.print("Press and hold any fruit to continue"); //play pressmesound
         playPressAndHold();
         delay(10000);
         gameType = readTouchInputs() + 1;
-          Serial.print(gameType);
         if(gameType != 0){
           // if anything is touched by this time
             pressStart = false;
@@ -54,30 +49,27 @@ void loop(){
         }
     }else{
         if(gameType == 1){
-          doSimon();
+          doClassic();
         }
         if(gameType == 2){
-          doClassic();
+          doSimon();
         }
         if(gameType == 3){
           /////doPiano();
         }
     }
     if(gameType == 1){
-      checkSimon();
+      checkClassic();
     }
     if(gameType == 2){
-      checkClassic();
+      checkSimon();
     }
     if(gameType == 3){
       //checkTeam();
     }
-    Serial.print("reading vmusic in main loop"); 
     readVMusic();
-    while (Serial.available())
+    if (Serial.available())
       mySerial.write(Serial.read());
-     delay(10);
-      
 }
 // clssic
 int hardness = 10000; //ten seconds to touch
@@ -97,10 +89,9 @@ void doClassic(){
   while(millis() < touchWindow){
      pinTouched = readTouchInputs();
      if(pinTouched == randNum){
-         long touch_time=millis();
-         playSound(randNum); //play fruit sound
-         delay(2000);
-         int timeBonus = ((touchWindow - touch_time)%100)*100 - (hardness/1000 +4) * 100 ;
+     playSound(randNum); //play fruit sound
+     delay(2000);
+     int timeBonus = ((touchWindow - millis())%100)*100 - (hardness/1000 +4) * 100 ;
           
               if(hardness > 1000){
                    hardness = hardness-1000;
@@ -192,22 +183,23 @@ void checkClassic(){
    if(strikes > 3){
       if(score > highscore){   
         playNoise('g', 5);//game over
-        delay(4000);
+        delay(6000);
         Serial.println("BOO! You SUCK at this game. But you got a highscore????"); //play meh
         Serial.print("New highscore: "); //play sound score
         playNoise('g',3);//your new high score is
-        delay(4000);
+        delay(5000);
+        delay(6000);
         Serial.println(score);
         playScore(score);//score
         highscore = score;
         newGameSet();
       }else{
         playNoise('g', 5); //game over
-        delay(4000);
+        delay(6000);
         Serial.println("BOO! You SUCK at this game."); //play badsound
         Serial.print("Score: "); //play sound score
         playNoise('g', 4);
-         delay(4000);
+         delay(11000);
         Serial.println(score);
         playScore(score);  //play sound score
         newGameSet();
@@ -218,10 +210,10 @@ void checkClassic(){
         Serial.println("WOOT!! You WIN at this game. AND you got a highscore!"); //play goodsound
         Serial.print("New highscore: ");
         playNoise('g',1); //you win
-        delay(4000);
+        delay(5000);
         Serial.println(score);
         playNoise('g',3); //your new high score is
-        delay(4000);
+        delay(2000);
         playScore(score); //play sound score
         highscore = score;
         newGameSet();
@@ -230,8 +222,6 @@ void checkClassic(){
         Serial.println("WOOT!! You WIN at this game."); //play goodsound
         Serial.print("Score: ");
         playNoise('g',1); //you win
-        delay(2000);
-        playNoise('g',4);
         delay(2000);
         playScore(score);
         Serial.println(score);
@@ -489,194 +479,142 @@ void playNoise(char type, int fruit){
   mySerial.write(filename);
   mySerial.write("\r");
 }
-int _i2caddr=0x5A;
-
 int readTouchInputs(){
-  int touched_pin=-1;
-  if(touch_status_changed){
-    touch_status_changed=false;
-    Serial.println("something happened!");
+  if(!checkInterrupt()){
+    
     //read the touch state from the MPR121
-    while(Wire.requestFrom(0x5A,2)!=2);
-
+    Wire.requestFrom(0x5A,2); 
+    
     byte LSB = Wire.read();
     byte MSB = Wire.read();
-
+    
     uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
 
+    
     for (int i=0; i < 12; i++){  // Check what electrodes were pressed
       if(touched & (1<<i)){
-
+      
         if(touchStates[i] == 0){
-          Serial.print("hi");
           //pin i was just touched
-          //if(i == 0){
-            Serial.print("pin ");
-            Serial.print(i);
-            Serial.println(" was just touched");
+          /*Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" was just touched");
+          Serial.print("pin 8:");*/
+          ////Serial.println(digitalRead(8));
+          //while(digitalRead(8)){
           //}
-          touched_pin=i;
-
-        }
-        else if(touchStates[i] == 1){
+          //mySerial.write("VPF s0.mp3\r");
+          ////Serial.println(mySerial.read());
+          return i;
+        }else if(touchStates[i] == 1){
           //pin i is still being touched
-        }
-
-        touchStates[i] = 1;
-      }
-      else{
+        }  
+      
+        touchStates[i] = 1;      
+      }else{
         if(touchStates[i] == 1){
-          //if(i == 0){
-            Serial.print("pin ");
-            Serial.print(i);
-            Serial.println(" is no longer being touched");
-          //}
+          Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" is no longer being touched");
+          
           //pin i is no longer being touched
-        }
+       }
+       
         touchStates[i] = 0;
       }
+    
     }
+    
   }
-  return touched_pin;
+  return -1;
 }
+
+
 
 
 void mpr121_setup(void){
-  writeRegister(MPR121_SOFTRESET, 0x63);
-  delay(1);
-  writeRegister(ELE_CFG, 0x00);
 
+  set_register(0x5A, ELE_CFG, 0x00); 
+  
   // Section A - Controls filtering when data is > baseline.
-  writeRegister(MHD_R, 0x01);
-  writeRegister(NHD_R, 0x01);
-  writeRegister(NCL_R, 0x0E);
-  writeRegister(FDL_R, 0x00);
+  set_register(0x5A, MHD_R, 0x01);
+  set_register(0x5A, NHD_R, 0x01);
+  set_register(0x5A, NCL_R, 0x00);
+  set_register(0x5A, FDL_R, 0x00);
 
   // Section B - Controls filtering when data is < baseline.
-  writeRegister(MHD_F, 0x01);
-  writeRegister(NHD_F, 0x05);
-  writeRegister(NCL_F, 0x01);
-  writeRegister(FDL_F, 0x00);
-
+  set_register(0x5A, MHD_F, 0x01);
+  set_register(0x5A, NHD_F, 0x01);
+  set_register(0x5A, NCL_F, 0xFF);
+  set_register(0x5A, FDL_F, 0x02);
+  
   // Section C - Sets touch and release thresholds for each electrode
-  writeRegister(ELE0_T, TOU_THRESH);
-  writeRegister(ELE0_R, REL_THRESH);
-
-  writeRegister(ELE1_T, TOU_THRESH); //apple
-  writeRegister(ELE1_R, REL_THRESH);
-
-  writeRegister(ELE2_T, TOU_THRESH); //2,5
-  writeRegister(ELE2_R, REL_THRESH);
-
-  writeRegister(ELE3_T, TOU_THRESH);//1,4
-  writeRegister(ELE3_R, REL_THRESH);
-
-  writeRegister(ELE4_T, TOU_THRESH);//5,7
-  writeRegister(ELE4_R, REL_THRESH);
-
-  writeRegister(ELE5_T, TOU_THRESH);
-  writeRegister(ELE5_R, REL_THRESH);
-
-  writeRegister(ELE6_T, TOU_THRESH);
-  writeRegister(ELE6_R, REL_THRESH);
-
-  writeRegister(ELE7_T, TOU_THRESH);//7,7
-  writeRegister(ELE7_R, REL_THRESH);
-
-  writeRegister(ELE8_T, TOU_THRESH);
-  writeRegister(ELE8_R, REL_THRESH);
-
-  writeRegister(ELE9_T, TOU_THRESH);
-  writeRegister(ELE9_R, REL_THRESH);
-
-  writeRegister(ELE10_T, TOU_THRESH);
-  writeRegister(ELE10_R, REL_THRESH);
-
-  writeRegister(ELE11_T, TOU_THRESH);
-  writeRegister(ELE11_R, REL_THRESH);
-
+  set_register(0x5A, ELE0_T, 0x44);
+  set_register(0x5A, ELE0_R, 0x44);
+ 
+  set_register(0x5A, ELE1_T, 0x33);
+  set_register(0x5A, ELE1_R, 0x33);
+  
+  set_register(0x5A, ELE2_T, TOU_THRESH);
+  set_register(0x5A, ELE2_R, REL_THRESH);
+  
+  set_register(0x5A, ELE3_T, TOU_THRESH);
+  set_register(0x5A, ELE3_R, REL_THRESH);
+  
+  set_register(0x5A, ELE4_T, TOU_THRESH);
+  set_register(0x5A, ELE4_R, REL_THRESH);
+  
+  set_register(0x5A, ELE5_T, TOU_THRESH);
+  set_register(0x5A, ELE5_R, REL_THRESH);
+  
+  set_register(0x5A, ELE6_T, TOU_THRESH);
+  set_register(0x5A, ELE6_R, REL_THRESH);
+  
+  set_register(0x5A, ELE7_T, TOU_THRESH);
+  set_register(0x5A, ELE7_R, REL_THRESH);
+  
+  set_register(0x5A, ELE8_T, TOU_THRESH);
+  set_register(0x5A, ELE8_R, REL_THRESH);
+  
+  set_register(0x5A, ELE9_T, TOU_THRESH);
+  set_register(0x5A, ELE9_R, REL_THRESH);
+  
+  set_register(0x5A, ELE10_T, TOU_THRESH);
+  set_register(0x5A, ELE10_R, REL_THRESH);
+  
+  set_register(0x5A, ELE11_T, TOU_THRESH);
+  set_register(0x5A, ELE11_R, REL_THRESH);
+  
   // Section D
   // Set the Filter Configuration
   // Set ESI2
-  //writeRegister(FIL_CFG, 16);
-   writeRegister(MPR121_CONFIG1, 0x1f); // default, 16uA charge current
-   writeRegister(MPR121_CONFIG2, 0x20); // 0.5uS encoding, 1ms period
-
+  set_register(0x5A, FIL_CFG, 0x04);
+  
   // Section E
   // Electrode Configuration
   // Set ELE_CFG to 0x00 to return to standby mode
-  writeRegister(ELE_CFG, 0x0C);  // Enables all 12 Electrodes
-
-  read_register(0x5A);
-
-
+  set_register(0x5A, ELE_CFG, 0x0C);  // Enables all 12 Electrodes
+  
+  
   // Section F
   // Enable Auto Config and auto Reconfig
-  /*writeRegister(ATO_CFG0, 0x0B);
-   writeRegister(ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   writeRegister(ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
-   writeRegister(ATO_CFGT, 0xB5);*/  // Target = 0.9*USL = 0xB5 @3.3V
-
-  //writeRegister(ELE_CFG, 0x0C);
-
+  /*set_register(0x5A, ATO_CFG0, 0x0B);
+  set_register(0x5A, ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   set_register(0x5A, ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
+  set_register(0x5A, ATO_CFGT, 0xB5);*/  // Target = 0.9*USL = 0xB5 @3.3V
+  
+  set_register(0x5A, ELE_CFG, 0x0C);
+  
 }
 
 
-void read_register(int address){
-  while(Wire.requestFrom(address, 6)!=6);    // request 6 bytes from slave device #2
-
-  while(Wire.available())    // slave may send less than requested
-  {
-    char c = Wire.read();    // receive a byte as character
-    Serial.print(c);         // print the character
-  }
-
-  delay(500); 
-}
-uint16_t  filteredData(uint8_t t) {
-  if (t > 12) return 0;
-  return readRegister16(MPR121_FILTDATA_0L + t*2);
+boolean checkInterrupt(void){
+  return digitalRead(irqpin);
 }
 
-uint16_t  baselineData(uint8_t t) {
-  if (t > 12) return 0;
-  uint16_t bl = readRegister8(MPR121_BASELINE_0 + t);
-  return (bl << 2);
-}
 
-uint16_t  touched(void) {
-  uint16_t t = readRegister16(MPR121_TOUCHSTATUS_L);
-  return t & 0x0FFF;
-}
-
-/*********************************************************************/
-
-
-uint8_t readRegister8(uint8_t reg) {
-    Wire.beginTransmission(_i2caddr);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    while (Wire.requestFrom(_i2caddr, 1) != 1);
-    return ( Wire.read());
-}
-
-uint16_t readRegister16(uint8_t reg) {
-    Wire.beginTransmission(_i2caddr);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    while (Wire.requestFrom(_i2caddr, 2) != 2);
-    uint16_t v = Wire.read();
-    v |=  ((uint16_t) Wire.read()) << 8;
-    return v;
-}
-
-/**************************************************************************/
-/*!
-    @brief  Writes 8-bits to the specified destination register
-*/
-/**************************************************************************/
-void writeRegister(uint8_t reg, uint8_t value) {
-    Wire.beginTransmission(_i2caddr);
-    Wire.write((uint8_t)reg);
-    Wire.write((uint8_t)(value));
+void set_register(int address, unsigned char r, unsigned char v){
+    Wire.beginTransmission(address);
+    Wire.write(r);
+    Wire.write(v);
     Wire.endTransmission();
 }
